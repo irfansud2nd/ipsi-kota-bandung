75e7b6841da0760e8aba3b46bb83ad1dd048ed70
irfansud2nd <irfansud2nd@gmail.com>
2024-06-11 13:23:39 +0700

    hide Home Dialog

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
   return (
     <div className="w-full h-full">
       <HomeBanner />
-      <HomeDialog />
+      {/* <HomeDialog /> */}
       <OngoingChampionships />
       <HomeMenu />
       <Announcement />
