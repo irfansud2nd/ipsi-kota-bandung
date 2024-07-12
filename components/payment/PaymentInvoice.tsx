f57d9f060977b544b6b8860a3039bdad840b141b
irfansud2nd <irfansud2nd@gmail.com>
2024-07-12 13:04:20 +0700

    payment invoice fix

--- components/payment/PaymentInvoice.tsx
+++ components/payment/PaymentInvoice.tsx
@@ -250,8 +250,8 @@ const PaymentInvoice = ({ payment }: { payment: Payment }) => {
       <Button onClick={generatePdf} className="mx-auto">
         Download
       </Button>
-      {/* <div className="absolute -z-50 top-0 right-full"> */}
-      <div>
+      <div className="absolute -z-50 top-0 right-full">
+        {/* <div> */}
         <div ref={printRef} className={`w-[1050px] h-fit`}>
           {datas.map((data, page) => (
             <div
