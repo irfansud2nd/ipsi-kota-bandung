6930f9ccfdc4f5f00f7426574566804e7e563cec
irfansud2nd <irfansud2nd@gmail.com>
2025-05-17 20:11:41 +0700

    fix: change proposal link

--- lib/event/eventConstants.ts
+++ lib/event/eventConstants.ts
@@ -363,7 +363,7 @@ export const championships: Championship[] = [
       },
     ],
     proposal:
-      "https://firebasestorage.googleapis.com/v0/b/ipsi-bandung.appspot.com/o/proposal%2FPROPOSAL%20BANDUNG%20OPEN%20PENCAK%20SILAT%20TOURNAMENT%202025.pdf?alt=media&token=ba2cd112-0f97-410c-a24b-f36cd2c0b3be",
+      "https://firebasestorage.googleapis.com/v0/b/ipsi-bandung.appspot.com/o/proposal%2FPROPOSAL%20BANDUNG%20OPEN%20PENCAK%20SILAT%20TOURNAMENT%202025.pdf?alt=media&token=49f649f0-1886-4d64-90a9-96015813c21e",
     status: {
       checkLimit: false,
     },
